4b7f9fe6af896ba5b4066f0a611e758d487f000d
AlexS <OstinKarlail@yandex.ru>
2025-05-05 20:15:15 +0500

    fix

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,40 +1,35 @@
 // src/routes/router.tsx
 import React from "react";
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Projects from "../pages/Projects";
 import Contact from "../pages/Contact";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-        {
-          path: "about",
-          element: <About />,
-        },
-        {
-          path: "projects",
-          element: <Projects />,
-        },
-        {
-          path: "contact",
-          element: <Contact />,
-        },
-      ],
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: '/alex-sav-portfolio'
-  }
-);
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "projects",
+        element: <Projects />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 export default router;
