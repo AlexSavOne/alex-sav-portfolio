b3a1210e8c352c5c39c8ccce32494f1adc156ed1
AlexS <OstinKarlail@yandex.ru>
2025-05-05 19:12:01 +0500

    fix: для деплоя

--- vite.config.js
+++ vite.config.js
@@ -3,32 +3,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/alex-sav-portfolio/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          styles: ['react-icons']
-        }
-      }
-    }
-  },
   server: {
     open: true,
-    port: 5173,
-    strictPort: true
-  }
+    hmr: true,
+  },
 })
